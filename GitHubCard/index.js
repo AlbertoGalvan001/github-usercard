2335db2f06294e4314ec745c1d535c02cf2e3be2
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-10-03 18:01:24 -0500

    mvp completed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const mainCard = document.querySelector('.cards')
+
+axios.get('https:api.github.com/users/AlbertoGalvan001')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,8 +36,17 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell', 'AlbertoGalvan001'];
 
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+    .then(response => {
+      const newCards = createCard(response.data);
+      profile.appendChild(newCards);
+    })
+})
+
+const profile = document.querySelector('.cards');
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -35,7 +56,7 @@ const followersArray = [];
     <h3 class="name">{users name}</h3>
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
-    <p>Profile:  
+    <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
@@ -45,8 +66,69 @@ const followersArray = [];
 </div>
 
 */
+function createCard(array01) {
 
-/* List of LS Instructors Github username's: 
+  //define  elements
+  const card = document.createElement('div');
+  const avatar = document.createElement('img');
+  const card_info = document.createElement('div');
+  const usersName = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const githubAddress = document.createElement('a');
+  const githubFollowers = document.createElement('p');
+  const githubFollowing = document.createElement('p');
+  const userBio = document.createElement('p');
+  const calendarDiv = document.createElement('div');
+  const break_ = document.createElement('br');
+
+  ///append
+  card.appendChild(avatar);
+  card.appendChild(card_info);
+  card_info.appendChild(usersName);
+  card_info.appendChild(username);
+  card_info.appendChild(location);
+  card_info.appendChild(profile);
+  profile.appendChild(githubAddress);
+  card_info.appendChild(githubFollowers);
+  card_info.appendChild(githubFollowing);
+  card_info.appendChild(userBio);
+  card_info.appendChild(calendarDiv);
+  card_info.appendChild(break_);
+
+  //add classes
+  card.classList.add("card");
+  card_info.classList.add("card-info");
+  usersName.classList.add("name");
+  username.classList.add("username");
+  calendarDiv.classList.add("calendar");
+
+
+  //content
+  avatar.src = array01.avatar_url;
+  usersName.textContent = array01.name;
+  username.textContent = array01.login;
+  location.textContent = `Location: ${array01.location}`;
+  githubAddress.href = array01.html_url;
+  githubAddress.textContent = array01.html_url;
+  githubFollowers.textContent = `Followers: ${array01.followers}`;
+  githubFollowing.textContent = `Following: ${array01.following}`;
+  userBio.textContent = `Bio: ${array01.bio}`;
+  profile.textContent = array01.profile;
+
+  // new GitHubCalendar(calendarDiv, username, {
+  //   global_stats: true,
+  //   responsive: true
+  // })
+
+
+
+  return card;
+
+}
+console.log(createCard);
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
